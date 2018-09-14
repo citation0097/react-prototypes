abee872d62429287e472257c0976e96d25b965bb
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-09-13 18:13:03 -0700

    functional components  - hank kim

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,37 @@
+import React from 'react';
+import Table from './table';
 
 
+
+export default () => {
+    // <h1>My first React functional component</h1>
+    
+    const students = [ 
+        { name: 'peter parker',
+        course: 'Intro photography',
+        grade: 100
+        },
+        { name: 'brouce banner',
+        course: 'psychology',
+        grade: 100
+        },
+        { name: 'tony start',
+        course: 'mechanical engineering',
+        grade: 100
+        }
+     ];
+
+     return (
+        <div className="container">
+            <div>
+                <h1>Student Grade Table</h1>
+                <Table data={students}/>
+            </div>
+        </div>
+     )
+    
+
+}
+
+
+// export default App;
